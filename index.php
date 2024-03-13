07e10cc27cad1cf009dbb7ca01c853fa96fc8209
maducz <160507829+maducz@users.noreply.github.com>
2024-03-13 12:14:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste do Xampp</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Página teste html</h1>
+    <p>teste pagina php</p>
+
+    <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+
+</body>
+</html>
